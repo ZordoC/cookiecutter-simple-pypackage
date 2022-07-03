2f37cc92a4b299fbb9326ec50ab963cfc3520d12
José Rodrigues <pedrogodinhoc@gmail.com>
2022-07-03 18:21:20 +0200

    Running code

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
-
 """Tests for `{{ cookiecutter.project_slug }}` package."""
-import pytest
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    import requests
-    return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
 def test_content(response):
