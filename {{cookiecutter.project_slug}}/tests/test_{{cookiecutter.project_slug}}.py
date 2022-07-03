b54c55d7fcc04dbeaa9d2370f899218e458b5712
José Rodrigues <pedrogodinhoc@gmail.com>
2022-07-03 18:00:02 +0200

    jinja2 updates

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -3,8 +3,6 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 import pytest
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
-
 
 @pytest.fixture
 def response():
@@ -20,4 +18,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
